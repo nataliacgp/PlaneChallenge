1bbbc7087265f5a5a60bfb879e4a13fd1f95d67c
natycg <checon@roverkids.org>
2024-12-06 13:39:33 -0500

    complete

--- PlaneChallenge/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ PlaneChallenge/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -18,12 +18,12 @@ public class PlayerControllerX : MonoBehaviour
     void FixedUpdate()
     {
         // get the user's vertical input
-        verticalInput = Input.GetAxis("Vertical");
+        verticalInput = Input.GetAxis("Vertical"); 
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * rotationSpeed * verticalInput * Time.deltaTime);
     }
 }
